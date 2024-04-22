b6051ac67fca63d088cbc7d39426b951c5175375
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-22 17:24:17 +0400

    a sample code to demonstrate on  SRP been added

--- SolidPrinciple.playground/Contents.swift
+++ SolidPrinciple.playground/Contents.swift
@@ -8,3 +8,31 @@ import UIKit
  */
 
 
+struct Product {
+    let price : Double
+}
+
+///sample violating the srp
+struct Invoice {
+    var products : [Product]
+    let id = NSUUID().uuidString
+    var discountPercentage : Double = 0.0
+    
+    var total : Double {
+        let total = products.map({$0.price}).reduce(0, { $0 + $1 })
+        let discountedAmount = total * (discountPercentage / 100)
+        return total - discountedAmount
+    }
+    
+    func printInvoice() {
+        print("--------")
+        print("Invoice id :\(id)")
+        print("Total cost :\(total)")
+        print("Discounts  :\(discountPercentage)")
+        print("--------")
+    }
+    
+    func saveInvoice() {
+        //save invoice locally or to database
+    }
+}
